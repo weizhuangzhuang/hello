38cd25ed3a5beda0a8b1dfda381682647873da86
weizhuangzhuang <l>
2020-07-10 10:17:42 +0800

    Initial commit

---
+++ src/main/java/com/wzz/hello/controller/Hello.java
@@ -0,0 +1,18 @@
+package com.wzz.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description : test  //描述
+ * @Author : zzwei  //作者
+ * @Date: 2020-07-10 09:57  //时间
+ */
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hahahaha";
+    }
+}
